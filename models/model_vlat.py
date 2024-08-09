89283a9f22a4134cbd149b857bfca9fc1a5829c4
CRKninni <kodicharanramtej@gmail.com>
2024-08-09 16:40:21 -0400

    updated_vlat with train condition

--- models/model_vlat.py
+++ models/model_vlat.py
@@ -36,7 +36,13 @@ torch.cuda.empty_cache()
 #         outputs = self.bert(text['input_ids'], attention_mask=text['attention_mask'])
 #         return outputs.last_hidden_state
 
-
+def tile(x, dim, n_tile):
+    init_dim = x.size(dim)
+    repeat_idx = [1] * x.dim()
+    repeat_idx[dim] = n_tile
+    x = x.repeat(*(repeat_idx))
+    order_index = torch.LongTensor(np.concatenate([init_dim * np.arange(n_tile) + i for i in range(init_dim)]))
+    return torch.index_select(x, dim, order_index.to(x.device))   
 
 
 class SelfAttention(nn.Module):
@@ -151,7 +157,64 @@ class VLAT(nn.Module):
         float_mask = binary_mask.float().masked_fill(binary_mask, float('-inf'))
         return float_mask
 
-    def forward(self, image, questions, answer, weights, k):
+    def rank_answer(self, question_states, question_atts, answer_ids, answer_atts, k):
+        
+        num_ques = question_states.size(0)
+        start_ids = answer_ids[0,0].repeat(num_ques,1) # bos token
+        
+        start_output = self.text_decoder(start_ids, 
+                                         encoder_hidden_states = question_states,
+                                         encoder_attention_mask = question_atts,                                      
+                                         return_dict = True, reduction = "none")              
+        logits = start_output.logits[:,0,:] # first token's logit
+        
+        # topk_probs: top-k probability 
+        # topk_ids: [num_question, k]        
+        answer_first_token = answer_ids[:,1]
+        prob_first_token = F.softmax(logits,dim=1).index_select(dim=1, index=answer_first_token) 
+        topk_probs, topk_ids = prob_first_token.topk(k,dim=1) 
+        
+        # answer input: [num_question*k, answer_len]                 
+        input_ids = []
+        input_atts = []
+        for b, topk_id in enumerate(topk_ids):
+            input_ids.append(answer_ids.index_select(dim=0, index=topk_id))
+            input_atts.append(answer_atts.index_select(dim=0, index=topk_id))
+        input_ids = torch.cat(input_ids,dim=0)  
+        input_atts = torch.cat(input_atts,dim=0)  
+
+        targets_ids = input_ids.masked_fill(input_ids == self.tokenizer.pad_token_id, -100)
+
+        # repeat encoder's output for top-k answers
+        question_states = tile(question_states, 0, k)
+        question_atts = tile(question_atts, 0, k)
+        
+        output = self.text_decoder(input_ids, 
+                                   attention_mask = input_atts, 
+                                   encoder_hidden_states = question_states,
+                                   encoder_attention_mask = question_atts,     
+                                   labels = targets_ids,
+                                   return_dict = True, reduction="none")                 
+        answer_loss = output.logits
+        answer_loss = answer_loss.view(input_ids.size(0),-1)
+        
+        # topk_prob: first token probability
+        topk_probs = topk_probs.view(-1,1)
+        log_probs = torch.cat([topk_probs.log(), -answer_loss],dim=1)
+
+        # re-calculate log probabilities for the answer sequences using chain rule
+        log_probs_sum = log_probs.sum(1)
+        log_probs_sum = log_probs_sum.view(num_ques,k)
+
+        topk_probs = F.softmax(log_probs_sum, dim=-1)
+        # get top-k after re-ranking
+        topk_probs, rerank_id = topk_probs.topk(k,dim=1) 
+        topk_ids = torch.gather(topk_ids, 1, rerank_id)    
+
+        return topk_ids, topk_probs
+
+    def forward(self, image, questions, answer, weights, k, train=True):
+        
         image_features = self.image_encoder(image) # (16. 3, 224, 224) -->  (batch_size, num_patches+1, hidden_dim) # (16, 197, 768) # 16*1,51,296*3
         image_atts = torch.ones(image_features.size()[:-1],dtype=torch.long).to(image.device)
         # print("aaa",questions.attention_mask.shape, image_atts.shape)
@@ -161,32 +224,41 @@ class VLAT(nn.Module):
         answer_targets = answer.input_ids.masked_fill(answer.input_ids == self.tokenizer.pad_token_id, -100) 
         
         # print("Encoder_decoder Block output", text_embeddings)
+        if train:
+            question_output = self.text_encoder_1(questions.input_ids, 
+                                                    attention_mask = questions.attention_mask, 
+                                                    encoder_hidden_states = image_embeddings,
+                                                    encoder_attention_mask = image_atts,                             
+                                                    return_dict = True)
+            question_states = []                
+            question_atts = []
+            # print(question_output.last_hidden_state.shape, text_embeddings.shape)  
+            for b, n in enumerate(k):
+                question_states += [question_output["last_hidden_state"][b]]*n
+                # question_states += [text_embeddings[b]]*n
+                question_atts += [questions.attention_mask[b]]*n 
+            question_states = torch.stack(question_states,0)    
+            question_atts = torch.stack(question_atts,0)   
 
-        question_output = self.text_encoder_1(questions.input_ids, 
+            answer_output = self.text_decoder(answer.input_ids, 
+                                                    attention_mask = answer.attention_mask, 
+                                                    encoder_hidden_states = question_states,
+                                                    encoder_attention_mask = question_atts,                  
+                                                    labels = answer_targets,
+                                                    return_dict = True, reduction = "none")
+            
+            loss = weights * answer_output.loss         
+            loss = loss.sum()/image.size(0)
+
+            # print("Loss:", loss)
+
+            return loss
+        else:
+            question_output = self.text_encoder_1(questions.input_ids, 
                                                 attention_mask = questions.attention_mask, 
                                                 encoder_hidden_states = image_embeddings,
-                                                encoder_attention_mask = image_atts,                             
-                                                return_dict = True)
-        question_states = []                
-        question_atts = []
-        # print(question_output.last_hidden_state.shape, text_embeddings.shape)  
-        for b, n in enumerate(k):
-            question_states += [question_output["last_hidden_state"][b]]*n
-            # question_states += [text_embeddings[b]]*n
-            question_atts += [questions.attention_mask[b]]*n 
-        question_states = torch.stack(question_states,0)    
-        question_atts = torch.stack(question_atts,0)   
-
-        answer_output = self.text_decoder(answer.input_ids, 
-                                                  attention_mask = answer.attention_mask, 
-                                                  encoder_hidden_states = question_states,
-                                                  encoder_attention_mask = question_atts,                  
-                                                  labels = answer_targets,
-                                                  return_dict = True, reduction = "none")
-        
-        loss = weights * answer_output.loss         
-        loss = loss.sum()/image.size(0)
-
-        print("Loss:", loss)
-
-        return image_embeddings, text_embeddings
+                                                encoder_attention_mask = image_atts, return_dict = True)
+            # print(question_output)                   
+            topk_ids, topk_probs = self.rank_answer(question_output["last_hidden_state"], questions.attention_mask, 
+                                                    answer.input_ids, answer.attention_mask, k) 
+            return topk_ids, topk_probs

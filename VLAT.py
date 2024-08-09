9be556557c645e6d0d8639b18480f7f795cf98d6
CRKninni <kodicharanramtej@gmail.com>
2024-08-09 15:57:23 -0400

    Vlat

---
+++ VLAT.py
@@ -0,0 +1,181 @@
+import argparse
+import os
+import  yaml
+import numpy as np
+import random
+import time
+import datetime
+import json
+from pathlib import Path
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import DataLoader
+import torch.backends.cudnn as cudnn
+import torch.distributed as dist
+
+from models.model_vlat import VLAT
+from models.vit import interpolate_pos_embed
+from transformers import BertTokenizer
+import sys
+import utils
+from dataset.utils import save_result
+from dataset import create_dataset, create_sampler, create_loader, vqa_collate_fn
+
+from scheduler import create_scheduler
+from optim import create_optimizer
+
+
+def train(model, data_loader, optimizer, tokenizer, epoch, warmup_steps, device, scheduler, config):
+    # train
+    model.train()  
+    
+    metric_logger = utils.MetricLogger(delimiter="  ")
+    metric_logger.add_meter('lr', utils.SmoothedValue(window_size=1, fmt='{value:.6f}'))
+    metric_logger.add_meter('loss', utils.SmoothedValue(window_size=1, fmt='{value:.4f}'))
+
+    header = 'Train Epoch: [{}]'.format(epoch)
+    print_freq = 50    
+    step_size = 100
+    warmup_iterations = warmup_steps*step_size  
+    
+    for i,(image, question, answer, weights, n) in enumerate(metric_logger.log_every(data_loader, print_freq, header)):
+        image, weights = image.to(device,non_blocking=True), weights.to(device,non_blocking=True)      
+        question_input = tokenizer(question, padding='longest', truncation=True, max_length=25, return_tensors="pt").to(device) 
+        answer_input = tokenizer(answer, padding='longest', return_tensors="pt").to(device) 
+        
+        if epoch>0 or not config['warm_up']:
+            alpha = config['alpha']
+        else:
+            alpha = config['alpha']*min(1,i/len(data_loader))
+
+        loss = model(image, question_input, answer_input, train=True, alpha=alpha, k=n, weights=weights)        
+        
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()    
+        
+        metric_logger.update(loss=loss.item())
+        metric_logger.update(lr=optimizer.param_groups[0]["lr"])
+        
+        if epoch==0 and i%step_size==0 and i<=warmup_iterations: 
+            scheduler.step(i//step_size) 
+            
+    # gather the stats from all processes
+    metric_logger.synchronize_between_processes()
+    print("Averaged stats:", metric_logger.global_avg())     
+    return {k: "{:.3f}".format(meter.global_avg) for k, meter in metric_logger.meters.items()} 
+
+
+@torch.no_grad()
+def evaluation(model, data_loader, tokenizer, device, config) :
+    # test
+    model.eval()
+            
+    metric_logger = utils.MetricLogger(delimiter="  ")
+    header = 'Generate VQA test result:'
+    print_freq = 50
+    
+    result = []
+    
+    answer_list = [answer+config['eos'] for answer in data_loader.dataset.answer_list]
+    answer_input = tokenizer(answer_list, padding='longest', return_tensors='pt').to(device)    
+        
+    for n, (image, question, question_id) in enumerate(metric_logger.log_every(data_loader, print_freq, header)):        
+        image = image.to(device,non_blocking=True)             
+        question_input = tokenizer(question, padding='longest', return_tensors="pt").to(device)        
+
+        topk_ids, topk_probs = model(image, question_input, answer_input, train=False, k=config['k_test'])      
+        
+        for ques_id, topk_id, topk_prob in zip(question_id, topk_ids, topk_probs):
+            ques_id = int(ques_id.item())          
+            _, pred = topk_prob.max(dim=0)
+            # _, pred_class = pred.max(1)
+            # accuracy = (targets==pred_class).sum() / targets.size(0)
+            result.append({"question_id":ques_id, "answer":data_loader.dataset.answer_list[topk_id[pred]]})   
+
+    return result
+
+
+
+
+def main(args, config):
+    utils.init_distributed_mode(args)
+    device = torch.device(args.device)
+    seed = args.seed + utils.get_rank()
+    torch.manual_seed(seed)
+    np.random.seed(seed)
+    random.seed(seed)
+    cudnn.benchmark = True
+    
+    start_epoch = 0
+    max_epoch = config['schedular']['epochs']
+    warmup_steps = config['schedular']['warmup_epochs']
+    
+    #### Dataset #### 
+    print("Creating vqa datasets")
+    datasets = create_dataset('vqa', config)   
+    
+    if args.distributed:
+        num_tasks = utils.get_world_size()
+        global_rank = utils.get_rank()            
+        samplers = create_sampler(datasets, [True, False], num_tasks, global_rank)         
+    else:
+        samplers = [None, None]
+    
+    train_loader, test_loader = create_loader(datasets,samplers,
+                                              batch_size=[config['batch_size_train'],config['batch_size_test']],
+                                              num_workers=[4,4],is_trains=[True, False], 
+                                              collate_fns=[vqa_collate_fn,None]) 
+    print(args.text_encoder)
+    tokenizer = BertTokenizer.from_pretrained(args.text_encoder)
+
+    #### Model #### 
+    print("Creating model")
+    model = VLAT()
+    model = model.to(device) 
+    
+    for epoch in range(start_epoch, max_epoch):
+        
+
+        for i,(image, question, answer, weights, n) in enumerate(train_loader):
+            image, weights = image.to(device,non_blocking=True), weights.to(device,non_blocking=True)      
+            question_input = tokenizer(question, padding='longest', truncation=True, max_length=25, return_tensors="pt").to(device) 
+            answer_input = tokenizer(answer, padding='longest', return_tensors="pt").to(device) 
+            loss = model(image, question_input, answer_input, weights, n)
+            sys.exit()
+        # dist.barrier()   
+  
+    vqa_result = evaluation(model, test_loader, tokenizer, device, config)        
+    result_file = save_result(vqa_result, args.result_dir, 'vqa_result_epoch%d'%epoch)
+                     
+ 
+    
+            
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--config', default='./configs/VQA.yaml') 
+    parser.add_argument('--checkpoint', default='') 
+    parser.add_argument('--output_dir', default='output/vqa')
+    parser.add_argument('--evaluate', action='store_true')    
+    parser.add_argument('--text_encoder', default='bert-base-uncased')
+    parser.add_argument('--text_decoder', default='bert-base-uncased')
+    parser.add_argument('--device', default='cuda')
+    parser.add_argument('--seed', default=42, type=int)
+    parser.add_argument('--world_size', default=1, type=int, help='number of distributed processes')    
+    parser.add_argument('--dist_url', default='env://', help='url used to set up distributed training')
+    parser.add_argument('--distributed', default=False, type=bool)
+    args = parser.parse_args()
+
+    config = yaml.load(open(args.config, 'r'), Loader=yaml.Loader)
+
+    args.result_dir = os.path.join(args.output_dir, 'result')
+
+    Path(args.output_dir).mkdir(parents=True, exist_ok=True)
+    Path(args.result_dir).mkdir(parents=True, exist_ok=True)
+        
+    yaml.dump(config, open(os.path.join(args.output_dir, 'config.yaml'), 'w'))    
+    
+    main(args, config)

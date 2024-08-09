6a9b987ddd6882cf1fd6872724f22f1e8a286d9e
CRKninni <kodicharanramtej@gmail.com>
2024-08-09 16:41:00 -0400

    updated_training

--- VLAT.py
+++ VLAT.py
@@ -86,7 +86,7 @@ def evaluation(model, data_loader, tokenizer, device, config) :
         image = image.to(device,non_blocking=True)             
         question_input = tokenizer(question, padding='longest', return_tensors="pt").to(device)        
 
-        topk_ids, topk_probs = model(image, question_input, answer_input, train=False, k=config['k_test'])      
+        topk_ids, topk_probs = model(image, question_input, answer_input, None, train=False, k=config['k_test'])      
         
         for ques_id, topk_id, topk_prob in zip(question_id, topk_ids, topk_probs):
             ques_id = int(ques_id.item())          
@@ -135,18 +135,38 @@ def main(args, config):
     print("Creating model")
     model = VLAT()
     model = model.to(device) 
-    
-    for epoch in range(start_epoch, max_epoch):
-        
+    arg_opt = utils.AttrDict(config['optimizer'])
+    optimizer = create_optimizer(arg_opt, model)
+    arg_sche = utils.AttrDict(config['schedular'])
+    lr_scheduler, _ = create_scheduler(arg_sche, optimizer)  
+    # for epoch in range(start_epoch, max_epoch):
+    #     for i, (image, question, answer, weights, n) in enumerate(train_loader):
+    #         image, weights = image.to(device, non_blocking=True), weights.to(device, non_blocking=True)
+    #         question_input = tokenizer(question, padding='longest', truncation=True, max_length=25, return_tensors="pt").to(device)
+    #         answer_input = tokenizer(answer, padding='longest', return_tensors="pt").to(device)
+    #         loss = model(image, question_input, answer_input, weights, n)
 
-        for i,(image, question, answer, weights, n) in enumerate(train_loader):
-            image, weights = image.to(device,non_blocking=True), weights.to(device,non_blocking=True)      
-            question_input = tokenizer(question, padding='longest', truncation=True, max_length=25, return_tensors="pt").to(device) 
-            answer_input = tokenizer(answer, padding='longest', return_tensors="pt").to(device) 
-            loss = model(image, question_input, answer_input, weights, n)
-            sys.exit()
-        # dist.barrier()   
-  
+    #         optimizer.zero_grad()
+
+    #         loss.backward()
+
+    #         optimizer.step()
+            
+    #         # Print loss every 50 batches
+    #         if (i + 1) % 50 == 0:
+    #             print(f"Epoch [{epoch + 1}/{max_epoch}], Batch [{i + 1}], Loss: {loss.item()}") 
+    #     save_obj = {
+    #             'model': model.state_dict(),
+    #             'optimizer': optimizer.state_dict(),
+    #             'lr_scheduler': lr_scheduler.state_dict(),
+    #             'config': config,
+    #             'epoch': epoch,
+    #         }
+    #     torch.save(save_obj, os.path.join(args.output_dir, 'checkpoint_%02d.pth'%epoch))  
+    checkpoint = torch.load("/home/beast/Desktop/Daniel/charan_anna/VLAT/output/vqa/checkpoint_04.pth")
+    model.load_state_dict(checkpoint['model'])
+    # start_epoch = checkpoint['epoch']
+    epoch = 5
     vqa_result = evaluation(model, test_loader, tokenizer, device, config)        
     result_file = save_result(vqa_result, args.result_dir, 'vqa_result_epoch%d'%epoch)
                      
